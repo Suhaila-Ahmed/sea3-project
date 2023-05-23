0b73fe87c971aac5f0c678e82342a2c77e773681
Suhaila-Ahmed <86728999+Suhaila-Ahmed@users.noreply.github.com>
2023-05-23 12:02:41 +0300

    Add files via upload

---
+++ v2_maxwithDisplay.ino
@@ -0,0 +1,280 @@
+#include <Wire.h>
+#include "MAX30100_PulseOximeter.h"
+#include "Adafruit_GFX.h"
+#include "OakOLED.h"
+#include <WiFi.h>
+
+const char ssid[] =  "Alaa";     // Replace with your WIFI SSID
+const char pass[] =  "stvh4016"; // Replace with your WIFI PASSWORD
+
+void send_event(const char *event);
+const char *host = "https://maker.ifttt.com/use/cdIpidFkquOryvi-HIIOE4";
+const char *privateKey = "cdIpidFkquOryvi-HIIOE4";
+
+
+const int MPU_addr=0x68;  // I2C address of the MPU-6050
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+float ax=0, ay=0, az=0, gx=0, gy=0, gz=0;
+boolean fall = false; //stores if a fall has occurred
+boolean trigger1=false; //stores if first trigger (lower threshold) has occurred
+boolean trigger2=false; //stores if second trigger (upper threshold) has occurred
+boolean trigger3=false; //stores if third trigger (orientation change) has occurred
+byte trigger1count=0; //stores the counts past since trigger 1 was set true
+byte trigger2count=0; //stores the counts past since trigger 2 was set true
+byte trigger3count=0; //stores the counts past since trigger 3 was set true
+int angleChange=0;
+
+#define REPORTING_PERIOD_MS     1000
+
+
+// Create a PulseOximeter object
+// Connections : SCL PIN - D1 , SDA PIN - D2 , INT PIN - D0
+
+PulseOximeter pox;
+///////////////////////////////////////////
+OakOLED oled;
+ 
+//char auth[] = "--------------------";             // Authentication Token Sent by Blynk
+//char ssid[] = "--------";        //WiFi SSID
+//char pass[] = "--------";        //WiFi Password
+ 
+ 
+//float BPM, SpO2;
+//uint32_t tsLastReport = 0;
+ 
+const unsigned char bitmap [] PROGMEM=
+{
+0x00, 0x00, 0x00, 0x00, 0x01, 0x80, 0x18, 0x00, 0x0f, 0xe0, 0x7f, 0x00, 0x3f, 0xf9, 0xff, 0xc0,
+0x7f, 0xf9, 0xff, 0xc0, 0x7f, 0xff, 0xff, 0xe0, 0x7f, 0xff, 0xff, 0xe0, 0xff, 0xff, 0xff, 0xf0,
+0xff, 0xf7, 0xff, 0xf0, 0xff, 0xe7, 0xff, 0xf0, 0xff, 0xe7, 0xff, 0xf0, 0x7f, 0xdb, 0xff, 0xe0,
+0x7f, 0x9b, 0xff, 0xe0, 0x00, 0x3b, 0xc0, 0x00, 0x3f, 0xf9, 0x9f, 0xc0, 0x3f, 0xfd, 0xbf, 0xc0,
+0x1f, 0xfd, 0xbf, 0x80, 0x0f, 0xfd, 0x7f, 0x00, 0x07, 0xfe, 0x7e, 0x00, 0x03, 0xfe, 0xfc, 0x00,
+0x01, 0xff, 0xf8, 0x00, 0x00, 0xff, 0xf0, 0x00, 0x00, 0x7f, 0xe0, 0x00, 0x00, 0x3f, 0xc0, 0x00,
+0x00, 0x0f, 0x00, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+ 
+void onBeatDetected()
+{
+    Serial.println("Beat Detected!");
+    oled.drawBitmap( 60, 20, bitmap, 28, 28, 1);
+    oled.display();
+}
+ ///////////////////////////////////////////////////////
+
+// Time at which the last beat occurred
+uint32_t tsLastReport = 0;
+
+// Callback routine is executed when a pulse is detected
+/*void onBeatDetected() {
+    Serial.println("♥ Beat!");
+}
+*/
+void setup() {
+  /////////////////////////////
+    Serial.begin(115200);
+
+    /////////mpu
+    Wire.beginTransmission(MPU_addr);
+    Wire.write(0x6B);  // PWR_MGMT_1 register
+    Wire.write(0);     // set to zero (wakes up the MPU-6050)
+    Wire.endTransmission(true);
+    /////////////////////////////////
+    oled.begin();
+    oled.clearDisplay();
+    oled.setTextSize(1);
+    oled.setTextColor(1);
+    oled.setCursor(0, 0);
+ 
+    oled.println("Initializing pulse oximeter..");
+    oled.display();
+    Serial.print("Initializing pulse oximeter..");
+
+    // Initialize sensor
+    if (!pox.begin()) {
+        Serial.println("FAILED");
+
+        oled.clearDisplay();
+         oled.setTextSize(1);
+         oled.setTextColor(1);
+         oled.setCursor(0, 0);
+         oled.println("FAILED");
+         oled.display();
+
+        for(;;);
+    } else {
+        oled.clearDisplay();
+         oled.setTextSize(1);
+         oled.setTextColor(1);
+         oled.setCursor(0, 0);
+         oled.println("SUCCESS");
+         oled.display();
+         Serial.println("SUCCESS");
+         pox.setOnBeatDetectedCallback(onBeatDetected);        
+    }
+
+  // Configure sensor to use 7.6mA for LED drive
+  pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
+
+    // Register a callback routine
+    pox.setOnBeatDetectedCallback(onBeatDetected);
+
+    ///////////////mpu wifi////////////////
+    WiFi.begin(ssid, pass);
+    while (WiFi.status() != WL_CONNECTED)
+     {
+    delay(500);
+    Serial.print(".");              
+     }
+  Serial.println("");
+  Serial.println("WiFi connected");
+ 
+}
+
+void loop() {
+  
+ mpu_read();
+ ax = (AcX-2050)/16384.00;
+ ay = (AcY-77)/16384.00;
+ az = (AcZ-1947)/16384.00;
+ gx = (GyX+270)/131.07;
+ gy = (GyY-351)/131.07;
+ gz = (GyZ+136)/131.07;
+ // calculating Amplitute vactor for 3 axis
+ float raw_amplitude = pow(pow(ax,2)+pow(ay,2)+pow(az,2),0.5);
+ int amplitude = raw_amplitude * 10;  // Mulitiplied by 10 bcz values are between 0 to 1
+ Serial.println(amplitude);
+ if (amplitude<=2 && trigger2==false){ //if AM breaks lower threshold (0.4g)
+   trigger1=true;
+   Serial.println("TRIGGER 1 ACTIVATED");
+   }
+ if (trigger1==true){
+   trigger1count++;
+   if (amplitude>=12){ //if AM breaks upper threshold (3g)
+     trigger2=true;
+     Serial.println("TRIGGER 2 ACTIVATED");
+     trigger1=false; trigger1count=0;
+     }
+ }
+ if (trigger2==true){
+   trigger2count++;
+   angleChange = pow(pow(gx,2)+pow(gy,2)+pow(gz,2),0.5); 
+   Serial.println(angleChange);
+   if (angleChange>=30 && angleChange<=400){ //if orientation changes by between 80-100 degrees
+     trigger3=true; trigger2=false; trigger2count=0;
+     Serial.println(angleChange);
+     Serial.println("TRIGGER 3 ACTIVATED");
+       }
+   }
+ if (trigger3==true){
+    trigger3count++;
+    if (trigger3count>=10){ 
+       angleChange = pow(pow(gx,2)+pow(gy,2)+pow(gz,2),0.5);
+       Serial.println(angleChange); 
+       if ((angleChange>=0) && (angleChange<=10)){ //if orientation changes remains between 0-10 degrees
+           fall=true; trigger3=false; trigger3count=0;
+           Serial.println(angleChange);
+             }
+       else{ //user regained normal orientation
+          trigger3=false; trigger3count=0;
+          Serial.println("TRIGGER 3 DEACTIVATED");
+       }
+     }
+  }
+ if (fall==true){ //in event of a fall detection
+   Serial.println("FALL DETECTED");
+   send_event("FALL DETECTION"); 
+   fall=false;
+   }
+ if (trigger2count>=6){ //allow 0.5s for orientation change
+   trigger2=false; trigger2count=0;
+   Serial.println("TRIGGER 2 DECACTIVATED");
+   }
+ if (trigger1count>=6){ //allow 0.5s for AM to break upper threshold
+   trigger1=false; trigger1count=0;
+   Serial.println("TRIGGER 1 DECACTIVATED");
+   }
+  delay(3000);
+    // Read from the sensor
+    pox.update();
+
+    // Grab the updated heart rate and SpO2 levels
+    if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
+        Serial.print("Heart rate:");
+        Serial.print(pox.getHeartRate());
+        Serial.print("bpm / SpO2:");
+        Serial.print(pox.getSpO2());
+        Serial.println("%");
+
+        oled.clearDisplay();
+        oled.setTextSize(1);
+        oled.setTextColor(1);
+        oled.setCursor(0,16);
+        oled.println(pox.getHeartRate());
+ 
+        oled.setTextSize(1);
+        oled.setTextColor(1);
+        oled.setCursor(0, 0);
+        oled.println("Heart BPM");
+ 
+        oled.setTextSize(1);
+        oled.setTextColor(1);
+        oled.setCursor(0, 30);
+        oled.println("Spo2");
+ 
+        oled.setTextSize(1);
+        oled.setTextColor(1);
+        oled.setCursor(0,45);
+        oled.println(pox.getSpO2());
+        oled.display();
+
+        tsLastReport = millis();
+    }
+}
+
+void mpu_read(){
+ Wire.beginTransmission(MPU_addr);
+ Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+ Wire.endTransmission(false);
+ Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+ AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+ AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+ AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+ Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+ GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+ GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+ GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+ }
+void send_event(const char *event)
+{
+  Serial.print("Connecting to "); 
+  Serial.println(host);
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("Connection failed");
+    return;
+  }
+  String url = "/trigger/";
+  url += event;
+  url += "/with/key/";
+  url += privateKey;
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  while(client.connected())
+  {
+    if(client.available())
+    {
+      String line = client.readStringUntil('\r');
+      Serial.print(line);
+    } else {
+      delay(50);
+    };
+  }
+Serial.println();
+  Serial.println("closing connection");
+  client.stop();
+}
+
